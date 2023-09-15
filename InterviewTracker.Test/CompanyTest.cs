dfc29257e79c38cd2b460b9cf050fa2963384513
Pathum Senaratna <pathusena@gmail.com>
2023-09-15 10:57:36 +0530

    Save company unit test cases Get all companies unit test cases

--- InterviewTracker.Test/CompanyTest.cs
+++ InterviewTracker.Test/CompanyTest.cs
@@ -1,4 +1,5 @@
 using InterviewTracker.BusinessLogic.Interfaces;
+using InterviewTracker.DataObject;
 using InterviewTracker.Test.MockData;
 using InterviewTracker.Web.Controllers;
 using Microsoft.AspNetCore.Http;
@@ -10,7 +11,7 @@ namespace InterviewTracker.Test
     public class CompanyTest
     {
         [Fact]
-        public void GetAllCompanies_ShouldReturn200Status()
+        public void GetAllCompanies_ShouldReturn200StatusWithData()
         {
             //Arrange
             var companyInterviewFacade = new Mock<ICompanyInterviewFacade>();
@@ -23,10 +24,13 @@ namespace InterviewTracker.Test
             //Assert
             var objectResult = Assert.IsType<OkObjectResult>(result);
             Assert.Equal(StatusCodes.Status200OK, objectResult.StatusCode);
+            List<CompanyDto>? companyList = objectResult.Value as List<CompanyDto>;
+            Assert.NotNull(companyList);
+            Assert.NotEmpty(companyList);
         }
 
         [Fact]
-        public void GetAllCompanies_ShouldReturn204Sttus() {
+        public void GetAllCompanies_ShouldReturn204Status() {
 
             //Arrange
             var companyInterviewFacade = new Mock<ICompanyInterviewFacade>();
@@ -40,6 +44,53 @@ namespace InterviewTracker.Test
             var objectResult = Assert.IsType<NoContentResult>(result);
             Assert.Equal(StatusCodes.Status204NoContent, objectResult.StatusCode);
 
+        }
+
+        [Fact]
+        public void GetAllCompanies_ShouldReturn500Status() { 
+            //Arrange
+            var companyInterviewFacade = new Mock<ICompanyInterviewFacade>();
+            companyInterviewFacade.Setup(x => x.GetAllCompanies()).Throws(new Exception("Internal Server Error"));
+            var sut = new WebApiController(companyInterviewFacade.Object);
+
+            //Act
+            var result = sut.GetAllCompanies();
+
+            //Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, objectResult.StatusCode);    
+        }
+
+        [Fact]
+        public void SaveCompany_ShouldReturn200StatusWithSavedCompany() { 
+            //Arrange
+            var companyInterviewFacade = new Mock<ICompanyInterviewFacade>();
+            companyInterviewFacade.Setup(x => x.SaveCompany(CompanyMockData.Company)).Returns(CompanyMockData.Company);
+            var sut = new WebApiController(companyInterviewFacade.Object);
+
+            //Act
+            var result = sut.SaveCompany(CompanyMockData.Company);
+
+            //Assert
+            var objectResult = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(objectResult.Value);
+            Assert.IsType<CompanyDto>(objectResult.Value);
+        }
+
+        [Fact]
+        public void SaveCompany_ShouldReturn500Status()
+        {
+            //Arrange
+            var companyInterviewFacade = new Mock<ICompanyInterviewFacade>();
+            companyInterviewFacade.Setup(x => x.SaveCompany(CompanyMockData.Company)).Throws(new Exception("Internal Server Error"));
+            var sut = new WebApiController(companyInterviewFacade.Object);
+
+            //Act
+            var result = sut.SaveCompany(CompanyMockData.Company);
+
+            //Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, objectResult.StatusCode);
         }
     }
 }
--- InterviewTracker.Test/MockData/CompanyMockData.cs
+++ InterviewTracker.Test/MockData/CompanyMockData.cs
@@ -9,6 +9,26 @@ namespace InterviewTracker.Test.MockData
 {
     public class CompanyMockData
     {
+        private static CompanyDto _Company = new CompanyDto { 
+            Id = -1,
+            Name = "Paragon Software (Pvt) Ltd",
+            Country = "Sri Lanka",
+            Date = DateTime.Now,
+            Description = "Description",
+            Phone = "0774817474",
+            Remarks = "Remarks Test"
+        };
+
+        public static CompanyDto Company
+        {
+            get {
+                return _Company;
+            }
+            set {
+                _Company = value;
+            }
+        }
+
         public static List<CompanyDto> GetCompanies() { 
             return new List<CompanyDto>() { 
                 new CompanyDto{ 
