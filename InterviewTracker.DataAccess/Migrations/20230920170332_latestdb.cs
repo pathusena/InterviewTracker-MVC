d062c6ee1a53017ba29dc235f7e59991550857a8
Pathum Senaratna <pathusena@gmail.com>
2023-09-20 22:38:12 +0530

    Db migrations

---
+++ InterviewTracker.DataAccess/Migrations/20230920170332_latestdb.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterviewTracker.DataAccess.Migrations
+{
+    public partial class latestdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "Company_Id_Sequence",
+                startValue: 2L);
+
+            migrationBuilder.CreateSequence(
+                name: "Interview_Id_Sequence");
+
+            migrationBuilder.CreateTable(
+                name: "Company",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true, defaultValueSql: "('')"),
+                    Country = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true, defaultValueSql: "('')"),
+                    Remarks = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true, defaultValueSql: "('')"),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "(getdate())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Company", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Interview",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: false),
+                    CompanyId = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<byte>(type: "tinyint", nullable: false),
+                    Remark = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interview", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Interview_Company",
+                        column: x => x.CompanyId,
+                        principalTable: "Company",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Interview_CompanyId",
+                table: "Interview",
+                column: "CompanyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Interview");
+
+            migrationBuilder.DropTable(
+                name: "Company");
+
+            migrationBuilder.DropSequence(
+                name: "Company_Id_Sequence");
+
+            migrationBuilder.DropSequence(
+                name: "Interview_Id_Sequence");
+        }
+    }
+}
